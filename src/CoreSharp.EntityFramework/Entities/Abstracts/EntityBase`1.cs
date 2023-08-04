3ad15b5f3a8d08a4e314bd760a847b1e15fb6028
Efthymios Koktsidis <efthymios.ks@gmail.com>
2023-08-04 14:17:12 +0300

    feat(entities): abstracts, split EntityBase into two models.

--- src/CoreSharp.EntityFramework/Entities/Abstracts/EntityBase`1.cs
+++ src/CoreSharp.EntityFramework/Entities/Abstracts/EntityBase`1.cs
@@ -1,40 +1,11 @@
 ﻿using CoreSharp.EntityFramework.Entities.Interfaces;
-using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Diagnostics;
-using JsonNet = Newtonsoft.Json;
-using TextJson = System.Text.Json;
 
 namespace CoreSharp.EntityFramework.Entities.Abstracts;
 
-[DebuggerDisplay("{DebuggerDisplay,nq}")]
-public abstract class EntityBase<TKey> : IEntity<TKey>
+public abstract class EntityBase<TKey> : EntityBase, IEntity<TKey>
 {
-    // Fields 
-    [DebuggerBrowsable(DebuggerBrowsableState.Never)]
-    [TextJson.Serialization.JsonIgnore]
-    [JsonNet.JsonIgnore]
-    private DateTime? _dateCreatedUtc;
-
-    [DebuggerBrowsable(DebuggerBrowsableState.Never)]
-    [TextJson.Serialization.JsonIgnore]
-    [JsonNet.JsonIgnore]
-    private DateTime? _dateModifiedUtc;
-
-    // Properties 
-    [DebuggerBrowsable(DebuggerBrowsableState.Never)]
-    [TextJson.Serialization.JsonIgnore]
-    [JsonNet.JsonIgnore]
-    private string DebuggerDisplay
-        => ToString();
-
-    [DebuggerBrowsable(DebuggerBrowsableState.Never)]
-    [NotMapped]
-    [TextJson.Serialization.JsonIgnore]
-    [JsonNet.JsonIgnore]
-    object IUniqueEntity.Id { get; set; } = default(TKey);
-
     [Key]
     [Column(Order = 0)]
     [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
@@ -44,28 +15,7 @@ public abstract class EntityBase<TKey> : IEntity<TKey>
         set => (this as IUniqueEntity).Id = value;
     }
 
-    [Column(Order = 1)]
-    public DateTime DateCreatedUtc
-    {
-        get => _dateCreatedUtc ?? DateTime.UtcNow;
-        set => _dateCreatedUtc = SetDateTimeKindToUtc(value);
-    }
-
-    [Column(Order = 2)]
-    public DateTime? DateModifiedUtc
-    {
-        get => _dateModifiedUtc;
-        set => _dateModifiedUtc = value is null ? null : SetDateTimeKindToUtc(value.Value);
-    }
-
     // Methods 
     public override string ToString()
         => $"{Id}";
-
-    /// <summary>
-    /// Avoid <see cref="DateTime.SpecifyKind(DateTime, DateTimeKind)"/> which converts (and ruins) the value.
-    /// Use <see cref="TimeZoneInfo.ConvertTimeToUtc(DateTime)"/> which only sets the <see cref="DateTime.Kind"/>.
-    /// </summary>
-    private static DateTime SetDateTimeKindToUtc(DateTime dateTime)
-        => TimeZoneInfo.ConvertTimeToUtc(dateTime);
 }
